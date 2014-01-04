65c535402243a064ceb06bbf786b6ca941c7cdb8
Rahul Trikha <rahul.trikha@gmail.com>
2014-01-04 16:55:07 +1030

    Added basic nav items

---
+++ app/scripts/models/nav_item.js
@@ -0,0 +1,4 @@
+Koi.NavItem = DS.Model.extend({
+  title: DS.attr('string'),
+  url: DS.attr('string')
+});
--- app/scripts/router.js
+++ app/scripts/router.js
@@ -2,6 +2,21 @@ Koi.Router.map(function () {
   this.resource('pages', { path: '/pages' }, function () {
     this.route('page', { path: '/:page_id' });
   });
+  this.resource('nav_items', { path: '/nav_items' }, function () {
+    this.route('nav_item', { path: '/:nav_item_id' });
+  });
+});
+
+Koi.NavItemsRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('navItem');
+  }
+});
+
+Koi.NavItemRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('navItem', params.nav_item_id);
+  }
 });
 
 Koi.PagesRoute = Ember.Route.extend({
