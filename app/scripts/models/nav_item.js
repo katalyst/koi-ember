04d4c8e20780ffcdd796f1dde3ea5c561984648c
Rahul Trikha <rahul.trikha@gmail.com>
2014-01-06 15:40:15 +1030

    Added key to nav items

--- app/scripts/models/nav_item.js
+++ app/scripts/models/nav_item.js
@@ -1,6 +1,7 @@
 Koi.NavItem = DS.Model.extend({
   title: DS.attr('string'),
   url: DS.attr('string'),
+  key: DS.attr('string'),
   children: DS.hasMany('navItem', { async: true }),
 
   formattedURL: function () {
